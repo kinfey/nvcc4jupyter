64de5e7fd5928738bd4e887c5c73c3749bb7deec
Kinfey <kinfeylo@microsoft.com>
2022-10-06 08:23:46 +0800

    master

--- nvcc_plugin.py
+++ nvcc_plugin.py
@@ -3,8 +3,8 @@ from v2.v2 import NVCCPluginV2 as NVCC_V2
 
 
 def load_ipython_extension(ip):
-    nvcc_plugin = NVCC_V1(ip)
-    ip.register_magics(nvcc_plugin)
+    azure_nvcc_plugin = NVCC_V1(ip)
+    ip.register_magics(azure_nvcc_plugin)
 
-    nvcc_plugin_v2 = NVCC_V2(ip)
-    ip.register_magics(nvcc_plugin_v2)
+    azure_nvcc_plugin_v2 = NVCC_V2(ip)
+    ip.register_magics(azure_nvcc_plugin)
