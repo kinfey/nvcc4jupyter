66738041a8b5cf44a56fff0b3b01b80d11d212a1
Kinfey <kinfeylo@microsoft.com>
2022-10-06 09:17:11 +0800

    master

--- azureml_nvcc_plugin.py
+++ azureml_nvcc_plugin.py
@@ -3,8 +3,8 @@ from v2.v2 import AzureNVCCPluginV2 as NVCC_V2
 
 
 def load_ipython_extension(ip):
-    azure_nvcc_plugin = NVCC_V1(ip)
-    ip.register_magics(azure_nvcc_plugin)
+    azureml_nvcc_plugin = NVCC_V1(ip)
+    ip.register_magics(azureml_nvcc_plugin)
 
-    azure_nvcc_plugin_v2 = NVCC_V2(ip)
-    ip.register_magics(azure_nvcc_plugin)
+    azureml_nvcc_plugin_v2 = NVCC_V2(ip)
+    ip.register_magics(azureml_nvcc_plugin_v2)
--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 
 setup(
-    name='AzureNVCCPlugin',
-    version='0.0.7',
+    name='AzureMLNVCCPlugin',
+    version='0.0.8',
     py_modules=['azureml_nvcc_plugin', 'v2.v2', 'v1.v1', 'common.helper'],
     license='LICENSE',
     description='Jupyter notebook plugin to run CUDA C/C++ code',
