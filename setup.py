93cc41004ccc1c2d9adf57522bd0ef969acf9399
Kinfey <kinfeylo@microsoft.com>
2022-10-06 08:37:57 +0800

    master

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='AzureNVCCPlugin',
-    version='0.0.52',
+    version='0.0.62',
     author='Andrei Nechaev',
     author_email='lyfaradey@yahoo.com',
     py_modules=['azureml_nvcc_plugin', 'v2.v2', 'v1.v1', 'common.helper'],
